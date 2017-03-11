399fd09f4e35b6a472bef34b427f7fa317cfa26f
Menon, Dhanya <dhanya.menon@pearson.com>
2017-03-10 21:32:12 -0800

    Rebase message amended Rebase message altered

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ window.onload = function(){
   console.log("trying this out");
   console.log("trying this out -1");
   console.log("trying this out 2");
-  console.log("trying this out 3");
-  document.write("this it my test ");
 
 
 }
